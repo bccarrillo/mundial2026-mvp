985960cfaddacb2a221a554f985633f55f1818a4
bcarrillo01 <bcarrillo@kreavox.com>
2026-02-10 05:13:19 -0500

    Update page.tsx

--- app/v2-demo/page.tsx
+++ app/v2-demo/page.tsx
@@ -1,7 +1,12 @@
 'use client';
 
 import V2Demo from '../../v2/pages/Demo';
+import { V2Provider } from '../../lib/V2Context';
 
 export default function V2DemoPage() {
-  return <V2Demo />;
+  return (
+    <V2Provider>
+      <V2Demo />
+    </V2Provider>
+  );
 }
